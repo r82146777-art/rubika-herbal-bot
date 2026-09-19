578b1388d5c2407b05027694b30b5cccb0bbe565
r82146777-art <r82146777@gmail.com>
2026-09-19 09:44:29 +0330

    حذف

--- get_updates.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-import json
-
-TOKEN = os.getenv("BOT_TOKEN")
-
-url = f"https://botapi.rubika.ir/v3/{TOKEN}/getUpdates"
-response = requests.post(url, json={"limit": 20}, timeout=30)
-print(json.dumps(response.json(), ensure_ascii=False, indent=2))
